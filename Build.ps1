ee63661c7cff6eb3c5af6caa35e0ab12b736309b
Jadyn <jadyn.wong@live.com>
2019-04-07 15:10:49 +0800

    build.ps1

--- Build.ps1
+++ Build.ps1
@@ -8,8 +8,14 @@ $revision = [convert]::ToInt32($revision, 10)
 
 echo $revision 
 
-dotnet test .\QQWryTest -c Release
+#dotnet test .\QQWryTest -c Release
+
+dotnet build QQWrySln.sln
 
 dotnet pack .\QQWry -o .\artifacts -p:Version=1.0.$revision --version-suffix=$revision
 
 dotnet pack .\QQWry.DependencyInjection -o .\artifacts -p:Version=1.0.$revision --version-suffix=$revision
+
+Set-Location .\artifacts
+
+Get-ChildItem
